02ef876b3b1626883a9ceec0a4a1d113d0b3db47
BhaskarJagi <bhaskarjagi.gmail.com>
2023-10-27 23:53:59 +0530

    second commit

--- src/components/Podcasts/AudioPlayer/index.js
+++ src/components/Podcasts/AudioPlayer/index.js
@@ -1,27 +1,82 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./styles.css";
+import { FaPlay, FaPause, FaVolumeUp, FaVolumeMute } from "react-icons/fa";
 
 function AudionPlayer({ image, audioSrc }) {
   const audioRef = useRef();
-  const [duration, setDuration] = useState();
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [isMute, setIsMute] = useState(true);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
   const [volume, setVolume] = useState(1);
 
-  function handleDuration(e) {
-    setDuration(e.target.value);
+//   function handleDuration(e) {
+//     setCurrentTime(e.target.value)
+//   }
+
+  function handleVolume(e) {
+    setVolume(e.target.value);
+    audioRef.current.volume = e.target.value;
   }
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      setIsPlaying(false);
+    } else {
+      setIsPlaying(true);
+    }
+  };
+
+  const toggleMute = () => {
+    if (isMute) {
+      setIsMute(false);
+    } else {
+      setIsMute(true);
+    }
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    if (isMute) {
+      audioRef.current.volume = 1;
+      setVolume(1)
+    } else {
+      audioRef.current.volume = 0;
+      setVolume(0)
+    }
+  }, [isMute]);
 
   return (
     <div className="custom-audio-player">
       <img src={image} className="display-image-player" />
       <audio ref={audioRef} src={audioSrc} />
       <div className="duration-flex">
-        <p>0:0</p>
+        <p onClick={togglePlay}> {isPlaying ? <FaPause /> : <FaPlay />}</p>
         <input
           type="range"
           className="duration-range"
-          onChange={handleDuration}
         />
-        <p>24:0</p>
+      </div>
+      <div className="volume-flex">
+        <p onClick={toggleMute}>
+          {isMute ? <FaVolumeUp /> : <FaVolumeMute />}
+        </p>
+        <input
+          type="range"
+          value={volume}
+          max={1}
+          min={0}
+          step={0.01}
+          className="volume-range"
+          onChange={handleVolume}
+        />
       </div>
     </div>
   );
